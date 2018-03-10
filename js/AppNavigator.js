01e45773ecd4acad2732d33f715999c50f652c5b
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-03-10 22:33:45 +0530

    Implement the language changer function inside AppNavigator.js

--- js/AppNavigator.js
+++ js/AppNavigator.js
@@ -17,6 +17,7 @@ import {
   ActionConst,
 } from 'react-native-router-flux';
 
+import i18n from './i18n/i18nConfig';
 
 const styles = StyleSheet.create({
   container: {
@@ -40,13 +41,27 @@ const reducerCreate = params => {
 };
 
 class AppNavigator extends Component {
+  
+  // point to this function when ever a language change is triggered
+  changeI18nLanguage(language) {
+    return new Promise((resolve, reject) => {
+      i18n.changeLanguage(language, (err, t) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });
+    });
+  }
+
   render() {
     return (
       <Router createReducer={reducerCreate}>
         <Scene key="root">
-          <Scene key="launch" component={Launch}/>
-          <Scene key="login" component={Login} initial hideNavBar/>
-          <Scene key="home" component={Home} title="Home" hideNavBar/>
+          <Scene key="launch" component={Launch} />
+          <Scene key="login" component={Login} initial hideNavBar />
+          <Scene key="home" component={Home} title="Home" hideNavBar />
         </Scene>
       </Router>
     );
