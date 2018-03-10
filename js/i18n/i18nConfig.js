75abd5bb259a04db5231fcf34f3ca920fd955221
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-03-10 22:32:41 +0530

    Add the configuration file for the 'i18next' library

---
+++ js/i18n/i18nConfig.js
@@ -0,0 +1,24 @@
+import i18next from 'i18next';
+import enTranslations from './locales/en/translations.json';
+import siTranslations from './locales/si-LK/translations.json';
+import taTranslations from './locales/ta-LK/translations.json';
+
+const instance = i18next.init({
+  fallbackLng: 'si-LK',
+  ns: ['translations'],
+  defaultNS: 'translations',
+  whitelist: ['en', 'si-LK', 'ta-LK'],
+  resources: {
+    'en': {
+      translations: enTranslations
+    },
+    'si-LK': {
+      translations: siTranslations
+    },
+    'ta-LK': {
+      translations: taTranslations
+    }
+  }
+});
+
+export default instance;
