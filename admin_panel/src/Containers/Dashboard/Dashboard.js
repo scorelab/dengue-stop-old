5cc8909fb012448f21e6b758c0dc75e55326f366
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-03-17 00:52:43 +0530

    Add table component in dashboard

--- admin_panel/src/Containers/Dashboard/Dashboard.js
+++ admin_panel/src/Containers/Dashboard/Dashboard.js
@@ -1,14 +1,20 @@
 import React , {Component} from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import PatientTableData from '../../Components/Table/Table';
 
-const styles={    
-};
+const styles=theme=>({    
+    table:{
+        margin:theme.spacing(2),
+        padding:theme.spacing(2),
+    }
+});
 
 class Dashboard extends Component{
     render(){
         const {classes} = this.props;
         return(
-            <div>
+            <div className={classes.table}>
+                <PatientTableData />
             </div>
         )
     }
