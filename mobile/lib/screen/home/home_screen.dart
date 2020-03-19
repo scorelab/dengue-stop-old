8636f7a9093716cd5eba9845fcea1a5dde36ad8a
hirantha <sahanhirantha@gmail.com>
2020-03-19 16:56:52 +0530

    screens binded with bottom navigation

--- mobile/lib/screen/home/home_screen.dart
+++ mobile/lib/screen/home/home_screen.dart
@@ -1,14 +1,40 @@
+import 'package:dengustop/screen/chat/chat_screen.dart';
+import 'package:dengustop/screen/map/map_screen.dart';
+import 'package:dengustop/screen/profile/profile_screen.dart';
+import 'package:dengustop/screen/settings/settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Widget> _screens = <Widget>[
+    MapScreen(),
+    ProfileScreen(),
+    ChatScreen(),
+    SettingsScreen(),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onSelectBottomNav(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Dengue Stop App"),
       ),
+      body: _screens.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
+        onTap: _onSelectBottomNav,
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
