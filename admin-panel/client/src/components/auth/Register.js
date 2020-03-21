d3cf0b065bb6c23779469dd7ae0c6ce1bf7b2fbc
Mithilesh Tiwari <shubhamtiwari.tiwari84@gmail.com>
2020-03-22 03:33:44 +0530

    Material UI added

--- admin-panel/client/src/components/auth/Register.js
+++ admin-panel/client/src/components/auth/Register.js
@@ -4,6 +4,14 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import classnames from "classnames";
+
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+
+
+
 class Register extends Component {
   constructor() {
     super();
@@ -12,9 +20,14 @@ class Register extends Component {
       email: "",
       password: "",
       password2: "",
-      errors: {}
+      errors: {},
+      type: 'password',
+      Icon: <VisibilityIcon/>,
+      cType: 'password',
+      cIcon: <VisibilityIcon/>
     };
   }
+  
 
   componentDidMount() {
     // If logged in and user navigates to Register page, should redirect them to dashboard
@@ -29,6 +42,20 @@ componentWillReceiveProps(nextProps) {
       });
     }
   }
+handleClick = () => this.setState(({type}) => ({
+    Icon: type === 'text' ? <VisibilityIcon/> : <VisibilityOffIcon/> ,
+    type: type === 'text' ? 'password' : 'text'
+  }))
+
+  confirmPassowrdShow = () => this.setState(({cType}) => ({
+    cIcon: cType === 'text' ? <VisibilityIcon/> : <VisibilityOffIcon/> ,
+    cType: cType === 'text' ? 'password' : 'text'
+    
+  }))
+
+passwordCheckHandler = () => {
+  alert(" Rules for a valid Password : \n 1- Minimum password length is 6. \n 2- Password must contain atleast an uppercase letter \n 3- Password must contain atleast an lowercase letter. \n 4- Password must contain atleast a digit. \n 5- Password must contain atleast a special character. (@,$,#,%,&)")
+}
 onChange = e => {
     this.setState({ [e.target.id]: e.target.value });
   };
@@ -89,33 +116,47 @@ return (
                 <label htmlFor="email">Email</label>
                 <span className="red-text">{errors.email}</span>
               </div>
-              <div className="input-field col s12">
+              <div className = "row" style = {{display:"inline"}}>
+              <div className="input-field col s12" >
                 <input
                   onChange={this.onChange}
                   value={this.state.password}
                   error={errors.password}
                   id="password"
-                  type="password"
+                  type={this.state.type}
                   className={classnames("", {
                     invalid: errors.password
                   })}
                 />
                 <label htmlFor="password">Password</label>
                 <span className="red-text">{errors.password}</span>
+                </div>
+                <div>
+                <IconButton aria-label="info" onClick = {this.handleClick}>
+                  {this.state.Icon}
+                </IconButton>
+                <IconButton aria-label="info" onClick = {this.passwordCheckHandler}>
+                <InfoIcon/>
+              </IconButton>
+                </div>
               </div>
+              
               <div className="input-field col s12">
                 <input
                   onChange={this.onChange}
                   value={this.state.password2}
                   error={errors.password2}
                   id="password2"
-                  type="password"
+                  type={this.state.cType}
                   className={classnames("", {
                     invalid: errors.password2
                   })}
                 />
                 <label htmlFor="password2">Confirm Password</label>
                 <span className="red-text">{errors.password2}</span>
+                <IconButton aria-label="info" onClick = {this.confirmPassowrdShow}>
+                  {this.state.cIcon}
+                </IconButton>
               </div>
               <div className="col s12" style={{ paddingLeft: "11.250px" }}>
                 <button
@@ -151,3 +192,4 @@ export default connect(
   mapStateToProps,
   { registerUser }
 )(withRouter(Register));
+
