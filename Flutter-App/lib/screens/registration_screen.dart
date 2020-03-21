f91f907d7ca32400967b03790c9e05d13a6f96b7
viradhanus <59219626+viradhanus@users.noreply.github.com>
2020-03-21 16:28:00 +0530

    updated

---
+++ Flutter-App/lib/screens/registration_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:dengue_stop/components/rounded_button.dart';
+import 'package:dengue_stop/constants.dart';
+import 'package:dengue_stop/screens/main_screen.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  static const String id = "registrations";
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your password'),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            RoundedButton(
+              title: 'Register',
+              color: Colors.blueAccent,
+              onPressed:  () {
+                Navigator.pushNamed(context, MainScreen.id);
+                //Implement registration functionality.
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
