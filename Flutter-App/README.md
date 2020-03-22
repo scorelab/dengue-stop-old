c08eaeeac182a87d6e7c2231aee4dbc0449b656c
viradhanus <59219626+viradhanus@users.noreply.github.com>
2020-03-22 08:55:24 +0530

    Implement modal_progress_hud
    A flutter widget modal_progress_hud is used to improve user experience.

--- Flutter-App/README.md
+++ Flutter-App/README.md
@@ -11,7 +11,7 @@ Access the complete project use the bellow link.
 
 This is a simple login and registration screens for the flutter app, dengue-stop. Previous logo of the react native app is used as the main logo of the flutter app. **Hero animation** is used to enhance UX.
 
-Firebase authentication is used to authenticate users to the app. Users trying to log in with badly-formated email address will be rejected and they should enter atleast 6 characters as a password to log in to the app. Logout button (**X**) is created in Main-Screen
+Firebase authentication is used to authenticate users to the app. Users trying to log in with badly-formated email address will be rejected and they should enter atleast 6 characters as a password to log in to the app. Logout button (**X**) is created in Main-Screen. A flutter widget **modal_progress_hud** is used to improve user experience. 
 
 For more information use the bellow link.
 
--- Flutter-App/lib/screens/registration_screen.dart
+++ Flutter-App/lib/screens/registration_screen.dart
@@ -3,7 +3,7 @@ import 'package:dengue_stop/components/rounded_button.dart';
 import 'package:dengue_stop/constants.dart';
 import 'package:dengue_stop/screens/main_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = "registrations";
@@ -14,6 +14,7 @@ class RegistrationScreen extends StatefulWidget {
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
 
+  bool showSpinner = false;
   final _auth = FirebaseAuth.instance;
   String email;
   String password;
@@ -22,65 +23,78 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body:ModalProgressHUD(
+            inAsyncCall: showSpinner,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Hero(
+                    tag: 'logo',
+                    child: Container(
+                      height: 200.0,
+                      child: Image.asset('images/logo.png'),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 48.0,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'Enter your email'),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  TextField(
+                    textAlign: TextAlign.center,
+                    obscureText: true,
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'Enter your password'),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  RoundedButton(
+                    title: 'Register',
+                    color: Colors.blueAccent,
+
+                    onPressed: () async {
+                      setState(() {
+                        showSpinner = true;
+                      });
+                      try {
+                        final newUser = await _auth
+                            .createUserWithEmailAndPassword(
+                            email: email, password: password);
+                        if (newUser != null) {
+                          Navigator.pushNamed(context, MainScreen.id);
+
+                          setState(() {
+                            showSpinner = false;
+                          });
+                        }
+                      } catch (e) {
+                        print(e);
+                      }
+                    },
+                  ),
+                ],
               ),
             ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your password'),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              title: 'Register',
-              color: Colors.blueAccent,
+          ),
 
-              onPressed:  () async {
-                try {
-                  print(email);
-                  print(password);
 
-                  final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-                  if(newUser != null){
-                    Navigator.pushNamed(context, MainScreen.id);
-                  }
-                } catch (e) {
-                  print(e);
-                }
-              },
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
