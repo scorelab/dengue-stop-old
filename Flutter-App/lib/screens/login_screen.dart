b3de3b9fdcc6db550e612d46121bae01a011b6ac
viradhanus <59219626+viradhanus@users.noreply.github.com>
2020-03-23 19:26:10 +0530

    fix UI overflow issues

--- Flutter-App/lib/screens/login_screen.dart
+++ Flutter-App/lib/screens/login_screen.dart
@@ -30,11 +30,13 @@ class _LoginScreenState extends State<LoginScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  Hero(
-                    tag: 'logo',
-                    child: Container(
-                      height: 200.0,
-                      child: Image.asset('images/logo.png'),
+                  Flexible(
+                    child: Hero(
+                      tag: 'logo',
+                      child: Container(
+                        height: 200.0,
+                        child: Image.asset('images/logo.png'),
+                      ),
                     ),
                   ),
                   SizedBox(
--- Flutter-App/lib/screens/registration_screen.dart
+++ Flutter-App/lib/screens/registration_screen.dart
@@ -31,11 +31,13 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  Hero(
-                    tag: 'logo',
-                    child: Container(
-                      height: 200.0,
-                      child: Image.asset('images/logo.png'),
+                  Flexible(
+                    child: Hero(
+                      tag: 'logo',
+                      child: Container(
+                        height: 200.0,
+                        child: Image.asset('images/logo.png'),
+                      ),
                     ),
                   ),
                   SizedBox(
