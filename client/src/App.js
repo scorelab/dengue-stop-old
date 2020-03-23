35d70f184f50e845a3e926796947b189ee1c8782
Rohan <rohanpd.work@gmail.com>
2020-03-23 13:06:25 +0530

    added login component page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import './App.css';
-import {Navbar} from 'react-bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/Login/Login";
 
 function App() {
-  return (
-    <div className="App" > 
-    <Navbar bg="light" style={{alignItems:'center'}}>
-    <Navbar.Brand  >Dengue Stop Admin Panel</Navbar.Brand>
-  </Navbar>
-    </div>
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>DengueStop Admin Panel</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+
+              <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={Login} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
-
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
