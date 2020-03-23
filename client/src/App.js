3f92341e32604c2f090faedecf365740672e4987
Rohan <rohanpd.work@gmail.com>
2020-03-23 13:38:46 +0530

    added register page fixes #188

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,22 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./components/Login/Login";
+import Register from './components/Register/Register';
 
 function App() {
   return (<Router>
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}>DengueStop Admin Panel</Link>
+          <Link className="navbar-brand" to={"/login"}>DengueStop Admin Panel</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+                <Link className="nav-link" to={"/login"}>Login</Link>
               </li>
               <li className="nav-item">
 
-              <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              <Link className="nav-link" to={"/register"}>Register</Link>
               </li>
             </ul>
           </div>
@@ -27,11 +28,11 @@ function App() {
       </nav>
 
       <div className="auth-wrapper">
-        <div className="auth-inner">
+        <div className="auth-inner"  style={{marginTop:"100px"}}>
           <Switch>
             <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={Login} />
+            <Route path="/login" component={Login} />
+            <Route  path="/register" component={Register} />
           </Switch>
         </div>
       </div>
