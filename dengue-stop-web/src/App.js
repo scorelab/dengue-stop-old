7c2feb64f3c9183934306236973f354c1d40f9dd
kali <saranraj@localhost.localdomain>
2020-03-25 01:49:46 -0400

    DashBoard Navbar Done

--- dengue-stop-web/src/App.js
+++ dengue-stop-web/src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Login from "./pages/Login/Login";
+import DashBoard from "./pages/DashBoard/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/login" exact component={Login} />
+          <Route path="/admin/home" exact component={DashBoard} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ dengue-stop-web/src/pages/DashBoard/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+export default Home;
